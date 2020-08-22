644f490d12098d7cd0977e56e16c22610c1117fd
JarvisLee <609089938@qq.com>
2020-08-23 02:13:47 +0800

    Word Average Model Pytorch Implementation

--- Deep_Neural_Network_with_Pytorch/Recurrent_Neural_Network_Demo/Sentiment_Classifier/WordAverageModel.py
+++ Deep_Neural_Network_with_Pytorch/Recurrent_Neural_Network_Demo/Sentiment_Classifier/WordAverageModel.py
@@ -67,7 +67,7 @@ class dataGenerator():
         # Spliting the training data into training data (70%) and development data (30%).
         trainData, devData = trainData.split(random_state = random.seed(1))
         # Creating the vocabulary.
-        textField.build_vocab(trainData, max_size = vocabularySize, vectors = 'glove.6B.100d', unk_init = torch.Tensor.normal_, vectors_cache = './Datasets/IMDB/')
+        textField.build_vocab(trainData, max_size = vocabularySize, vectors = 'glove.6B.100d', unk_init = torch.Tensor.normal_, vectors_cache = './Datasets/IMDB/glove.6B.100d/')
         labelField.build_vocab(trainData)
         # Getting the train, dev and test sets.
         trainSet, devSet, testSet = torchtext.data.BucketIterator.splits((trainData, devData, testData), batch_size = batchSize, device = device)
