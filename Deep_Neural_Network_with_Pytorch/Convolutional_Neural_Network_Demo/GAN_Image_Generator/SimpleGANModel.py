8d278a991b8542bcc0473abfb7fc2876c0d98012
JarvisLee <609089938@qq.com>
2020-09-20 12:30:43 +0800

    Simple GAN Model Pytorch Implementation

--- Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANModel.py
+++ Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANModel.py
@@ -4,30 +4,18 @@
 #   Project Name:       SimpleGANModel.py
 #   Description:        Build a simple GAN model to generating the image in MNIST datastes.
 #   Model Description:  Input               ->  MNIST Images and Fake Images
-#                       Generator           ->  Conv2d      ->  Kernel Size:    (3, 3)
-#                                                           ->  Channel:        10 
-#                                                           ->  Same Padding
-#                                           ->  MaxPool2d   ->  Kernel Size:    (2, 2)
-#                                                           ->  Stride:         2
+#                       Generator           ->  Linaer (latentSize, 200)
 #                                           ->  ReLu
-#                                           ->  Conv2d      ->  Kernel Size:    (1, 1)
-#                                                           ->  Channel:        5
+#                                           ->  Linear (200, 400)
 #                                           ->  ReLu
-#                                           ->  Linear      ->  Linear(980, 784)
-#                       Discriminator       ->  Conv2d      ->  Kernel Size:    (3, 3)
-#                                                           ->  Channel:        5
-#                                                           ->  Same Padding
-#                                           ->  MaxPool2d   ->  Kernel Size:    (2, 2)
-#                                                           ->  Stride:         2
-#                                           ->  ReLu
-#                                           ->  Conv2d      ->  Kernel Size:    (5, 5)
-#                                                           ->  Channel Size:   10
-#                                                           ->  Same Padding
-#                                           ->  MaxPool2d   ->  Kernel Size:    (2, 2)
-#                                                           ->  Stride:         2
-#                                           ->  ReLu
-#                                           ->  Linear      ->  Linear(490, 200)
-#                                           ->  Linear      ->  Linear(200, 1)      
+#                                           ->  Linear (400, imageSize)
+#                                           ->  tanh
+#                       Discriminator       ->  Linear (imageSize, 400)
+#                                           ->  LeakyReLu (0.2)
+#                                           ->  Linear (400, 200)
+#                                           ->  LeakyReLu (0.2)
+#                                           ->  Linear (200, 1)
+#                                           ->  Sigmoid    
 #============================================================================================#
 
 # Importing the necessary library.
@@ -58,9 +46,14 @@ else:
 
 # Setting the hyperparameters.
 # The value of the learning rate.
-learningRate = 2e-4
+learningRateG = 2e-4
+learningRateD = 2e-4
 # The value of the batch size.
 batchSize = 32
+# The value of the latent size.
+latentSize = 100
+# The value of the image size.
+imageSize = 28 * 28
 # The value of the epoches.
 epoches = 30
 
@@ -74,18 +67,28 @@ class dataLoader():
             download = False
         else:
             download = True
+        # Setting the transformation method.
+        transformMethod = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean = (0.5,),
+                    std = (0.5,)
+                )
+            ]
+        )
         # Getting the training data.
         trainData = datasets.MNIST(
             root = './Datasets/',
             train = True,
-            transform = transforms.ToTensor(),
+            transform = transformMethod,
             download = download
         )
         # Getting the training set.
         trainSet = DataLoader(
             trainData,
             batch_size = batchSize,
-            shuffle = True
+            shuffle = True,
+            drop_last = True
         )
         # Returning the training sets.
         return trainSet
@@ -97,24 +100,19 @@ class Generator(nn.Module):
         # Inheriting the super constructor.
         super(Generator, self).__init__()
         # Defining the model.
-        self.conv1 = nn.Conv2d(1, 10, (3, 3), padding = 1)
-        self.pool1 = nn.MaxPool2d((2, 2), 2)
-        self.conv2 = nn.Conv2d(10, 5, (1, 1))
-        self.linear = nn.Linear(980, 784)
+        self.linear1 = nn.Linear(latentSize, 200)
+        self.linear2 = nn.Linear(200, 400)
+        self.linear3 = nn.Linear(400, imageSize)
     # Defining the forward.
     def forward(self, x):
-        # [batchSize, 1, 28, 28] -> [batchSize, 10, 28, 28]
-        x = self.conv1(x)
-        # [batchSize, 10, 28, 28] -> [batchSize, 10, 14, 14]
-        x = self.pool1(x)
+        # [batchSize, latentSpace] -> [batchSize, 200]
+        x = self.linear1(x)
         x = F.relu(x)
-        # [batchSize, 10, 14, 14] -> [batchSize, 5, 14, 14]
-        x = self.conv2(x)
+        # [batchSize, 200] -> [batchSize, 400]
+        x = self.linear2(x)
         x = F.relu(x)
-        # [batchSize, 5, 14, 14] -> [batchSize, 980]
-        x = x.reshape(-1, 980)
-        # [batchSize, 980] -> [batchSize, 784]
-        x = self.linear(x)
+        # [batchSize, 400] -> [batchSize, imageSize]
+        x = self.linear3(x)
         # Returning the result.
         return torch.tanh(x)
 
@@ -125,30 +123,19 @@ class Discriminator(nn.Module):
         # Inhertiting the super constructor.
         super(Discriminator, self).__init__()
         # Defining the model.
-        self.conv1 = nn.Conv2d(1, 5, (3, 3), padding = 1)
-        self.pool1 = nn.MaxPool2d((2, 2), 2)
-        self.conv2 = nn.Conv2d(5, 10, (5, 5), padding = 2)
-        self.pool2 = nn.MaxPool2d((2, 2), 2)
-        self.linear1 = nn.Linear(490, 200)
-        self.linear2 = nn.Linear(200, 1)
+        self.linear1 = nn.Linear(imageSize, 400)
+        self.linear2 = nn.Linear(400, 200)
+        self.linear3 = nn.Linear(200, 1)
     # Defining the forward.
     def forward(self, x):
-        # [batchSize, 1, 28, 28] -> [batchSize, 5, 28, 28]
-        x = self.conv1(x)
-        # [batchSize, 5, 28, 28] -> [batchSize, 5, 14, 14]
-        x = self.pool1(x)
-        x = F.relu(x)
-        # [batchSize, 5, 14, 14] -> [batchSize, 10, 14, 14]
-        x = self.conv2(x)
-        # [batchSize, 10, 14, 14] -> [batchSize, 10, 7, 7]
-        x = self.pool2(x)
-        x = F.relu(x)
-        # [batchSize, 10, 7, 7] -> [batchSize, 490]
-        x = x.reshape(-1, 490)
-        # [batchSize, 490] -> [batchSize, 200]
+        # [batchSize, latentSpace] -> [batchSize, 400]
         x = self.linear1(x)
-        # [batchSize, 200] -> [batchSize, 1]
+        x = F.leaky_relu(x, 0.2)
+        # [batchSize, 400] -> [batchSize, 200]
         x = self.linear2(x)
+        x = F.leaky_relu(x, 0.2)
+        # [batchSize, 200] -> [batchSize, 1]
+        x = self.linear3(x)
         # Returning the result.
         return torch.sigmoid(x)
 
@@ -156,18 +143,18 @@ class Discriminator(nn.Module):
 class SimpleGANModelNN():
     # Training the model.
     @staticmethod
-    def trainer(trainSet, epoches, batchSize, learningRate):
+    def trainer(trainSet, epoches, batchSize, learningRateG, learningRateD, latentSize, imageSize):
         # Creating the models.
         G = Generator().to(device)
         D = Discriminator().to(device)
         # Creating the optimizer.
-        optimG = optim.Adam(G.parameters(), lr = learningRate)
-        optimD = optim.Adam(D.parameters(), lr = learningRate)
+        optimG = optim.Adam(G.parameters(), lr = learningRateG)
+        optimD = optim.Adam(D.parameters(), lr = learningRateD)
         # Creating the loss function.
         loss = nn.BCELoss()
         # Initializing the label.
-        trueLabel = torch.ones([batchSize, 1]).to(device)
-        fakeLabel = torch.zeros([batchSize, 1]).to(device)
+        trueLabel = torch.ones(batchSize, 1).to(device)
+        fakeLabel = torch.zeros(batchSize, 1).to(device)
         # Initializing the train loss.
         trainLossesG = 0
         trainLossesD = 0
@@ -178,12 +165,12 @@ class SimpleGANModelNN():
             trainLossG = []
             # Training the model.
             for i, (data, _) in enumerate(trainSet):
-                # Sending the data into corresponding device.
-                data = data.to(device)
                 # Initializting the latent space.
-                latentSpace = torch.rand_like(data).to(device)
+                latentSpace = torch.randn(batchSize, latentSize).to(device)
+                # Sending the data into corresponding device.
+                data = data.reshape(data.shape[0], imageSize).to(device)
                 # Getting the fake image.
-                fakeImage = G(latentSpace).reshape((data.shape[0], 1, 28, 28))
+                fakeImage = G(latentSpace)
                 # Training the discriminator.
                 # Computing the prediction.
                 truePrediction = D(data)
@@ -202,10 +189,14 @@ class SimpleGANModelNN():
                 optimD.step()
 
                 # Training the generator.
+                # Initializting the latent space.
+                latentSpace = torch.randn(batchSize, latentSize).to(device)
+                # Getting the fake image.
+                fakeImage = G(latentSpace)
                 # Computing the prediction.
                 fakePrediction = D(fakeImage)
                 # Computing the loss.
-                costG= loss(fakePrediction, trueLabel)
+                costG = loss(fakePrediction, trueLabel)
                 # Storing the loss
                 trainLossG.append(costG.item())
                 # Clearing the previous gradient descent.
@@ -215,11 +206,8 @@ class SimpleGANModelNN():
                 costG.backward()
                 # Updating the parameters.
                 optimG.step()
-            # Selecting the best model.
-            if (trainLossesG == 0 and trainLossesD == 0) or ((np.sum(trainLossG) / len(trainLossG)) < trainLossesG or (np.sum(trainLossD) / len(trainLossD)) < trainLossesD):
-                # Saving the model.
-                torch.save(G.train().state_dict(), './Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/Simple_GAN_Image_Generator/SimpleGANModel.pt')
-                print("Generator Saved")
+            # Saving the model.
+            torch.save(G.train().state_dict(), './Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANGenerator.pt')
             # Storing the losses.
             trainLossesG = (np.sum(trainLossG) / len(trainLossG))
             trainLossesD = (np.sum(trainLossD) / len(trainLossD))
@@ -240,6 +228,7 @@ if __name__ == "__main__":
     #         # Getting the image.
     #         image = transform(data[j])
     #         # Plotting the image.
+    #         plt.title("Real MNIST Image")
     #         plt.imshow(image, cmap = plt.cm.gray)
     #         plt.show()
     #         # Getting the command.
@@ -255,29 +244,41 @@ if __name__ == "__main__":
     # while cmd != 'Exit':
     #     if cmd == 'T':
     #         # Training the model.
-    #         SimpleGANModelNN.trainer(trainSet, epoches, batchSize, learningRate)
+    #         SimpleGANModelNN.trainer(trainSet, epoches, batchSize, learningRateG, learningRateD, latentSize, imageSize)
     #         # Getting the command.
     #         cmd = input("Please input the command ('T' for training, 'E' for evaluating, 'Exit' for quit): ")
     #     elif cmd == 'E':
     #         try:
     #             # Loading the model.
     #             model = Generator()
-    #             model.load_state_dict(torch.load('./Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/Simple_GAN_Image_Generator/SimpleGANModel.pt'))
+    #             model.load_state_dict(torch.load('./Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANGenerator.pt'))
     #             # Sending the model into the corresponding device.
     #             model = model.to(device).eval()
+    #             # Creating the latent space.
+    #             latentSpace = torch.randn(batchSize, latentSize).to(device)
+    #             # Getting the fake image.
+    #             fakeImages = model(latentSpace).reshape((batchSize, 1, 28, 28)).to('cpu')
     #             # Plotting the image.
-    #             for i in range(batchSize):
-    #                 # Creating the latent space.
-    #                 latentSpace = torch.randn((1, 1, 28, 28)).to(device)
-    #                 # Getting the fake image.
-    #                 fakeImage = model(latentSpace).reshape((1, 1, 28, 28)).to('cpu')
-    #                 # Setting the transformation.
-    #                 transform = transforms.ToPILImage()
-    #                 # Getting the image.
-    #                 image = transform(fakeImage[0])
-    #                 # Plotting the image.
-    #                 plt.imshow(image, cmap = plt.cm.gray)
-    #                 plt.show()
+    #             while True:
+    #                 # Getting the image number.
+    #                 i = input("Please input a image number (%d <= number <= %d and 'Exit' for quit): " % (1, batchSize))
+    #                 # Indicate the input.
+    #                 if i == 'Exit':
+    #                     break
+    #                 else:
+    #                     # Indicate the input value.
+    #                     try:
+    #                         i = eval(i)
+    #                         # Setting the transformation.
+    #                         transform = transforms.ToPILImage()
+    #                         # Getting the image.
+    #                         image = transform(fakeImages[i-1])
+    #                         # Plotting the image.
+    #                         plt.title("Generated MNIST Image")
+    #                         plt.imshow(image, cmap = plt.cm.gray)
+    #                         plt.show()
+    #                     except:
+    #                         print("Please input a valid number!!!")
     #             # Getting the command.
     #             cmd = input("Please input the command ('T' for training, 'E' for evaluating, 'Exit' for quit): ")
     #         except:
