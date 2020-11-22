c98f43e4be0409abd0dd3546e759a820ebd373f2
JarvisLee <609089938@qq.com>
2020-11-22 10:51:56 +0800

    DCGAN Model Data Preprocessor

---
+++ Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/DCGANModel/Utils/DataPreprocessor.py
@@ -0,0 +1,39 @@
+#============================================================================================#
+#   Copyright:      JarvisLee
+#   Date:           2020/11/21
+#   File Name:      dataPreprocessor.py
+#   Description:    This file is used to set the data preprocessor.
+#============================================================================================#
+
+# Importing the necessary library.
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# Creating the dataloader.
+class dataLoader():
+    # Creating the method to get the train data.
+    @staticmethod
+    def CELEBA(root, batchSize):
+        # Setting the transformation method.
+        transform = transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean = (0.5, 0.5, 0.5),
+                std = (0.5, 0.5, 0.5)
+            )
+        ])
+        # Getting the data.
+        trainData = datasets.ImageFolder(
+            root = root,
+            transform = transform
+        )
+        # Getting the training set.
+        trainSet = DataLoader(
+            trainData,
+            batch_size = batchSize,
+            shuffle = True,
+            drop_last = True
+        )
+        # Returning the train set.
+        return trainSet
