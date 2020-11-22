0bdddb0fc15c84ac15a2e250c3980990c72ce777
JarvisLee <609089938@qq.com>
2020-11-23 00:05:21 +0800

    SimpleGAN Model Trainer

--- Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANModel/Trainer.py
+++ Deep_Neural_Network_with_Pytorch/Convolutional_Neural_Network_Demo/GAN_Image_Generator/SimpleGANModel/Trainer.py
@@ -155,6 +155,8 @@ def trainer(trainSet):
         )
         # Saving the model.
         torch.save(G.train().state_dict(), Cfg.modelDir + '/SimpleGANGenerator.pt')
+    # Saving the graph.
+    vis.save(envs = ['SimpleGANModel'])
 
 # Training the model.
 if __name__ == "__main__":
